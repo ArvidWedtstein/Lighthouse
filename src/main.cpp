8b436f3d086ed2c82014c167dd0609b0430138ed
ArvidWedtstein <71834553+ArvidWedtstein@users.noreply.github.com>
2026-08-13 17:11:46 +0200

    Add LED control and debounce logic for light sensor readings

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@ bool serialActive = false;
 // ---- Pin definitions ----
 const int RELAY_PIN = 6;
 const int LIGHT_SENSOR_PIN = A0; 
+const int LED_PIN = LED_BUILTIN; 
 
 // ---- Settings ----
 const int DARK_THRESHOLD = 5;  
@@ -15,6 +16,11 @@ const unsigned long LoopDelay = 1000UL; // 1s
 
 const unsigned long ON_DURATION_CYCLES   = (2UL * 60UL * 60UL) / 8UL;   
 const unsigned long MAX_COOLDOWN_CYCLES = (12UL * 60UL * 60UL) / 8UL; 
+
+// ---- Debounce settings ----
+// Each cycle is ~8s (one watchdog wake). Requiring 4 consecutive consistent
+// readings means a transition needs to hold for ~32s before it's trusted.
+const int DEBOUNCE_CYCLES = 4;
 
 // ---- Relay logic level ----
 const int RELAY_ON = LOW;
@@ -30,6 +36,10 @@ enum State {
 State currentState = IDLE;
 unsigned long cycleCounter = 0; 
 bool isDark = false;
+int ledState = LOW;
+
+int darkStreak = 0;
+int lightStreak = 0;
 
 ISR(WDT_vect) {
   // Used as physical wakeup alarm clock
@@ -57,7 +67,9 @@ void enterDeepSleep() {
 
 void setup() {
   pinMode(RELAY_PIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
   digitalWrite(RELAY_PIN, RELAY_OFF);
+
 
   setupWatchdog();
 }
@@ -69,6 +81,24 @@ void loop() {
     isDark = true;
   } 
   else if (lightLevel >= LIGHT_THRESHOLD) {
+    isDark = false;
+  }
+
+  if (lightLevel <= DARK_THRESHOLD) {
+    darkStreak++;
+    lightStreak = 0;
+  } else if (lightLevel >= LIGHT_THRESHOLD) {
+    lightStreak++;
+    darkStreak = 0;
+  } else {
+    // In the dead zone between thresholds -> doesn't count toward either streak
+    darkStreak = 0;
+    lightStreak = 0;
+  }
+
+  if (darkStreak >= DEBOUNCE_CYCLES) {
+    isDark = true;
+  } else if (lightStreak >= DEBOUNCE_CYCLES) {
     isDark = false;
   }
 
@@ -112,6 +142,13 @@ void loop() {
       }
       break;
   }
+
+  if (isDark) {
+    ledState = HIGH;
+  } else {
+    ledState = LOW;
+  }
+  digitalWrite(LED_PIN, ledState);
 
   enterDeepSleep();
 }
